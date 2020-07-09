fa163732879abc43229f7ce1a2ac4dc388decad5
heruideid <2641180078@qq.com>
2020-07-09 22:25:49 +0800

    .

--- src/test/java/com/example/springboottest/repository/BookRepositoryTest.java
+++ src/test/java/com/example/springboottest/repository/BookRepositoryTest.java
@@ -1,7 +1,5 @@
 package com.example.springboottest.repository;
 
-import com.example.springboottest.entity.Book;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,16 +10,4 @@ class BookRepositoryTest {
     @Autowired
     private BookRepository bookRepository;
 
-    @Test
-    void findAll(){
-        System.out.println(bookRepository.findAll());
-    }
-
-    @Test
-    void save(){
-        Book book=new Book();
-        book.setAuthor("张三");
-        book.setName("李白集");
-        Book book1=bookRepository.save(book);
-    }
 }
